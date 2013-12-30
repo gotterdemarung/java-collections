55421ff2d80f4ddb19b5158630dd151daef3ee77
gotterdemarung <gotterdemarung@gmail.com>
2013-12-30 09:37:13 +0200

    gradle compatibility

--- src/test/java/me/gotter/collections/ChainNodeTest.java
+++ src/test/java/me/gotter/collections/ChainNodeTest.java
@@ -1,11 +1,11 @@
+package me.gotter.collections;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-
-import me.gotter.collections.ChainNode;
 
 public class ChainNodeTest {
 
